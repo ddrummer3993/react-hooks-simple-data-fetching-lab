18acb72565763f7a1836f2eb3ac97752fb6028b3
David Drummer <david.b.drummer@gmail.com>
2022-04-28 16:06:39 -0400

    commit tree c082a725c484427154e0440f46cb7da5bf4b0ded

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,24 @@
-// create your App component here
+import React, { useEffect, useState } from "react";
+
+function App() {
+    
+    const [isLoading, setLoading] = useState(false);
+    const [dogData, setDogData] = useState("");
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+            .then((resp) => resp.json())
+            .then((data) => {
+                setDogData(data);
+                setLoading(true);
+            });
+    }, []);
+
+    if(!isLoading) return <p>Loading...</p>
+    
+    return (
+        <img src={dogData.message} alt={"A Random Dog"}></img>
+    )
+};
+
+export default App;
